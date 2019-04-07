83e53e3420fdaa8ea0ce449573e4ed942ba391cf
teshio <teshio@hotmail.com>
2019-04-07 18:59:02 +0100

    basic edit form

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,168 @@
-var app = angular.module('myApp', []);
+(function(angular) {
+  'use strict';
 
-app.factory('ReadingService', function($http) {
-  return {
-    setToken: function(token) {
-      this.token = token;
-    },
-    getReadings: function(onSuccess) {
+  var app = angular.module('myApp', ['ngRoute', 'ngSanitize']);
 
-      $http({
-        url: 'https://healthreading.azurewebsites.net/api/readings/',
-        method: "GET",
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'bearer ' + this.token
-        }
+  app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'loginController'
       })
-      .then(onSuccess);
+      .when('/readings', {
+        templateUrl: 'views/readings.html',
+        controller: 'readingController',
+        resolve: {}
+      })
+      .when('/readings/add', {
+        templateUrl: 'views/readingForm.html',
+        controller: 'readingEditController',
+        resolve: {}
+      })
+      .when('/readings/edit/:id', {
+        templateUrl: 'views/readingForm.html',
+        controller: 'readingEditController',
+        resolve: {}
+      })
+      .otherwise({
+        templateUrl: 'views/home.html'
+      });
+
+    $locationProvider.html5Mode(true);
+  }]);
+
+
+  app.factory('ReadingService', function($http) {
+    return {
+      setToken: function(token) {
+        this.token = token;
+      },
+      getReadings: function(onSuccess) {
+
+        if (this.token && this.token.length > 0) {
+          $http({
+              url: 'https://healthreading.azurewebsites.net/api/readings/',
+              method: "GET",
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'bearer ' + this.token
+              }
+            })
+            .then(onSuccess);
+        } else {
+          onSuccess();
+        }
+
+      },
+      logout: function() {
+        this.token = null;
+      }
 
     }
-  }
-});
+  });
 
-app.controller('readingController', function($scope, $http, $httpParamSerializer, ReadingService) {
-  var initialise = function() {
-    ReadingService.getReadings(function(data) {
-      console.log(data.data);
-      $scope.data = data.data;
+  app.controller('readingEditController', function($scope, $http, $httpParamSerializer, ReadingService, $route, $location, $routeParams) {
+    //var readingId = $routeParams.id;
+    //console.log('id:' + readingId);
+  });
+
+
+  app.controller('mainController', function($scope, $http, $httpParamSerializer, ReadingService, $route, $location, $routeParams) {
+    $scope.$on('loggedin', function() {
+      $scope.isLoggedIn = true;
     });
-  };
-  $scope.$on('loggedin', initialise);
-});
 
-app.controller('loginController', function($scope, $rootScope, $http, $httpParamSerializer, ReadingService) {
-  $scope.test = "hi"
-  $scope.login = {
-    username: '',
-    password: ''
-  };
+    $scope.isActive = function(viewLocation) {
+      return viewLocation === $location.path();
+    };
 
-  $scope.run = function() {
-    $scope.loading = true;
-    $http({
-        url: 'https://healthreading.azurewebsites.net/Token',
-        method: "POST",
-        data: $httpParamSerializer({
-          'grant_type': 'password',
-          'username': $scope.login.username,
-          'password': $scope.login.password
-        }),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded' // Note the appropriate header
+    $scope.classActive = function(viewLocation) {
+      return $scope.isActive(viewLocation) ? 'active' : '';
+    }
+
+    $scope.logout = function() {
+      $scope.isLoggedIn = false;
+      ReadingService.logout();
+      $location.path('/login');
+    };
+
+  });
+
+  app.controller('readingController', function($scope, $http, $httpParamSerializer, ReadingService) {
+    var initialise = function() {
+      ReadingService.getReadings(function(data) {
+        console.log(data.data);
+        $scope.data = data.data;
+      });
+    };
+    $scope.$on('loggedin', initialise);
+
+    $scope.deleteReading = function(reading) {
+      var data = $scope.data;
+      $scope.data = data.filter(function(value, index, arr) {
+        return value.id != reading.id;
+      });
+    }
+
+    $scope.editReading = function(reading) {
+
+      $('#readingDialog').modal();
+      reading.readingDate = null;
+      $scope.reading = reading;
+      $rootScope.reading = reading;
+      window.setTimeout(2000, function(){
+
+      });
+/*
+      $uibModal.open({
+        templateUrl: 'views/readingForm.html',
+        controller: function($scope) {
+          debugger;
         }
-      })
-      .then(function(response) {
-          $scope.login.token = response.data.access_token;
-          ReadingService.setToken($scope.login.token);
-          $scope.output = $scope.login.token;
-          $rootScope.$broadcast('loggedin');
-          $scope.loading = false;
-        },
-        function(response) { // optional
-          console.log(response);
-          $scope.loading = false;
-          $scope.loginMessage = response.data.error_description;
-        });
-  }
-});
+      });*/
+      //$scope.reading = reading;
+
+    }
+
+    initialise();
+
+  });
+
+  app.controller('loginController', function($scope, $rootScope, $http, $httpParamSerializer, ReadingService, $location) {
+
+    $scope.login = {
+      username: '',
+      password: ''
+    };
+
+    $scope.run = function() {
+      $scope.loading = true;
+      $http({
+          url: 'https://healthreading.azurewebsites.net/Token',
+          method: "POST",
+          data: $httpParamSerializer({
+            'grant_type': 'password',
+            'username': $scope.login.username,
+            'password': $scope.login.password
+          }),
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then(function(response) {
+            $scope.login.token = response.data.access_token;
+            ReadingService.setToken($scope.login.token);
+            $scope.output = $scope.login.token;
+            $rootScope.$broadcast('loggedin');
+            $location.path('/readings');
+            $scope.loading = false;
+          },
+          function(response) { // optional
+            console.log(response);
+            $scope.loading = false;
+            $scope.loginMessage = response.data.error_description;
+          });
+    }
+  });
+
+})(window.angular);
